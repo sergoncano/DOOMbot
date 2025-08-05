fda4f613c4c742d5991a59d19bfa8cce28933c21
sergoncano <36008895+sergoncano@users.noreply.github.com>
2025-08-05 22:54:48 +0200

    Added a few more commands and improved readability of the code

--- bot.py
+++ bot.py
@@ -11,16 +11,54 @@ class BotInstance:
         self.player = message.author
         self.channel = message.channel
 
-    async def initialize_gif(self):
+    async def initialize(self):
         self.gifmessage = await self.channel.send(
             DOOM_GIF_URL
-            + f"\nCurrent player: {self.player.name}"
-            + '\nuse "doom end" to exit'
         )
 
-    async def delete_self(self):
+    async def doom_exit(self):
         await self.gifmessage.delete()
         del players[self.player.id]
+
+    @staticmethod
+    async def doom_help(channel):
+        help_message = '''
+**Commands**
+**Doom init**: start a DOOM game
+**Doom back**: go back to the previous state of the game
+**Doom exit**: exit your ongoing doom game
+**Game controls**:
+    -W: Move forth
+    -S: Move back
+    -A: Move left
+    -D: Move right
+    -Q: Fire weapon
+    -E: Open door
+All commands are case insensitive'''
+        await channel.send(help_message)
+
+    async def doom_back(self):
+        if self.gifmessage.content[-7] == "/":
+            return
+        self.gifmessage = await self.gifmessage.edit(self.gifmessage.content[:-7] + self.gifmessage.content[-6:])
+
+    async def doom_control(self, message):
+        controls = ["w","a","s","d","e","q"]
+        for key in controls:
+            if message.content == key:
+                self.gifmessage = await self.gifmessage.edit(
+                    content=self.gifmessage.content.replace("i", key + "i", 1)
+                )
+                await message.delete()
+
+    async def doom_command(self, message):
+        text = message.content.lower()
+        if text == "doom exit":
+            await self.doom_exit()
+            await message.delete()
+        elif text == "doom back":
+            await self.doom_back()
+            await message.delete()
 
     async def on_message(self, message):
         print(f"Recieved {message.content}")
@@ -28,43 +66,14 @@ class BotInstance:
         if message.author == bot.user:
             return
 
-        if message.author == self.player and message.content == "doom end":
-            await self.delete_self()
+        if message.author == self.player and message.content.lower().startswith("doom"):
+            await self.doom_command(message)
             return
 
-        if message.author != self.player:
-            return
+        if message.author == self.player:
+            await self.doom_control(message)
 
-        if message.content == "w":
-            self.gifmessage = await self.gifmessage.edit(
-                content=self.gifmessage.content.replace("i", "wi", 1)
-            )
-            await message.delete()
-        elif message.content == "a":
-            self.gifmessage = await self.gifmessage.edit(
-                content=self.gifmessage.content.replace("i", "ai", 1)
-            )
-            await message.delete()
-        elif message.content == "s":
-            self.gifmessage = await self.gifmessage.edit(
-                content=self.gifmessage.content.replace("i", "si", 1)
-            )
-            await message.delete()
-        elif message.content == "d":
-            self.gifmessage = await self.gifmessage.edit(
-                content=self.gifmessage.content.replace("i", "di", 1)
-            )
-            await message.delete()
-        elif message.content == "e":
-            self.gifmessage = await self.gifmessage.edit(
-                content=self.gifmessage.content.replace("i", "ei", 1)
-            )
-            await message.delete()
-        elif message.content == "q":
-            self.gifmessage = await self.gifmessage.edit(
-                content=self.gifmessage.content.replace("i", "qi", 1)
-            )
-            await message.delete()
+        
 
 if __name__ == "__main__":
     dotenv.load_dotenv()
@@ -85,11 +94,14 @@ if __name__ == "__main__":
     @bot.event
     async def on_message(message):
         id = message.author.id
-        if message.content == "doom init":
+        text = message.content.lower()
+        if text == "doom init":
             if id in players:
                 return
             players[id] = BotInstance(message)
-            await players[id].initialize_gif()
+            await players[id].initialize()
+        elif text == "doom help":
+            await BotInstance.doom_help(message.channel)
         else:
             if id in players:
                 await players[id].on_message(message)
